2b69ec74f16d6bbd79ff4124dae2f51887bd06fb
Rapli Kunz <raffikun1203@gmail.com>
2024-06-16 09:51:22 +0000

    update compiler

--- compile-zero.sh
+++ compile-zero.sh
@@ -1,19 +1,16 @@
-#!/bin/sh
-# Compile script for Zero Two kernel
+#!/bin/bash
+# Compile script for Arisuu kernel
 # Copyright (c) RapliVx Aka Rafi Aditya
 
-# setup dir
-PHONE="Ulysse"
-DEFCONFIG=nexus_defconfig
-COMPILERDIR="/workspace/kernel_xiaomi_mi8937/toolchain"
-AK3_DIR="/workspace/kernel_xiaomi_mi8937/AnyKernel3"
-CLANG="Proton Clang"
-WORK_DIR=$(pwd)
-KERN_IMG="${WORK_DIR}/out/arch/arm64/boot/Image-gz.dtb"
-ZIPNAME="Arisuu-Kernel-[$VERSION]-$(date '+%Y%m%d-%H%M').zip"
+# Setup
 export KBUILD_BUILD_USER=Rapli
-export KBUILD_BUILD_HOST=PotatoServer
-
+export KBUILD_BUILD_HOST=Projects-Anime
+DEVICE="Ulysse"
+CLANG="Proton Clang"
+SECONDS=0
+TC_DIR="/workspace/kernel_xiaomi_mi8937/toolchain"
+AK3_DIR="/workspace/kernel_xiaomi_surya/Android/AK3"
+DEFCONFIG="nexus_defconfig"
 
 # Header
 cyan="\033[96m"
@@ -46,71 +43,61 @@ echo -e " "
 read -rp "[?] Insert kernel version: " VERSION
 echo -e " "
 
+# Build Script
+
 function clean() {
     echo -e "\n"
-    echo -e "$red [!] CLEANING UP \\033[0m"
+    echo -e "$red << cleaning up >> \\033[0m"
     echo -e "\n"
     rm -rf out
     make mrproper
 }
 
-# Make Defconfig
-
 function build_kernel() {
-    export PATH="$COMPILERDIR/bin:$PATH"
-    make -j$(nproc --all) O=out ARCH=arm64 ${DEFCONFIG}
-    if [ $? -ne 0 ]
-then
-    echo -e "\n"
-    echo -e "$red [!] BUILD FAILED \033[0m"
-    echo -e "\n"
-else
-    echo -e "\n"
-    echo -e "$green==================================\033[0m"
-    echo -e "$green= [!] START BUILD ${DEFCONFIG}\033[0m"
-    echo -e "$green==================================\033[0m"
-    echo -e "\n"
-fi
-
-# Build Start Here
-
-make -j$(nproc --all) ARCH=arm64 O=out \
+export PATH="$TC_DIR/bin:$PATH"
+make O=out ARCH=arm64 $DEFCONFIG
+make -j$(nproc --all) O=out ARCH=arm64 \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                       CC=clang \
-                      LLVM=1- 
+                      LLVM=1
 
-    if [ -e "$KERN_IMG" ]; then
-		echo -e "$green=============================================\033[0m"
-		echo -e "$green= [+] Zipping up Kernel ...\033[0m"
-		echo -e "$green=============================================\033[0m"
-    if [ -d "$AK3_DIR" ]; then
+# Kernel Dir
+kernel="out/arch/arm64/boot/Image.gz-dtb"
+
+# ZIPNAME
+ZIPNAME="ZeroTwo-Kernel-[$VERSION]-$(date '+%Y%m%d-%H%M').zip"
+
+if [ -f "$kernel" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+	if [ -d "$AK3_DIR" ]; then
 		cp -r $AK3_DIR AnyKernel3
 	elif ! git clone -q https://github.com/RapliVx/AnyKernel3.git -b ulysse; then
-			echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+		echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+		exit 1
 	fi
-		cp $KERN_IMG AnyKernel3
-		cd AnyKernel3
-		git checkout ulysse &> /dev/null
-		zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
-		cd ..
-		rm -rf AnyKernel3
+	cp $kernel AnyKernel3
+	rm -rf out/arch/arm64/boot
+	cd AnyKernel3
+	git checkout ulysse &> /dev/null
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	rm -rf AnyKernel3
 fi
 
-    if [ -e "$ZIPNAME" ]; then
+if [ -f $ZIPNAME ] ; then
     echo -e "$green===========================\033[0m"
     echo -e "$green=  SUCCESS COMPILE KERNEL \033[0m"
-    echo -e "$green=  Device    : $PHONE \033[0m"
+    echo -e "$green=  Device    : $DEVICE \033[0m"
     echo -e "$green=  Defconfig : $DEFCONFIG \033[0m"
     echo -e "$green=  Toolchain : $CLANG \033[0m"
-    echo -e "$green=  Codename  : $VERSION \033[0m"
-    echo -e "$green=  Out       : $ZIPNAME \033[0m"
+    echo -e "$green=  Out       : $ZIPNAME \033[0m "
     echo -e "$green=  Completed in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) \033[0m "
     echo -e "$green=  Have A Brick Day Nihahahah \033[0m"
     echo -e "$green===========================\033[0m"
-    else
-    echo -e "$red [!] FIX YOUR KERNEL SOURCE BRUH !?\033[0m"
-    fi
+else
+echo -e "$red! FIX YOUR KERNEL SOURCE BRUH !?\033[0m"
+fi
 }
 
 # execute
